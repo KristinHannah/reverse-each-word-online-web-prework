ee4b73f5fdd648f0e7ec03ae7a9a04e6d03b1215
Kristin Kraemer <kristinhannahk@gmail.com>
2019-04-18 23:52:55 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,15 @@
+def reverse_each_word(sentence1)
+  sentarray = sentence1.split(/ /)
+  reverseArray = []
+  sentarray.each do | word |
+    reverseArray.push(word.reverse)
+  end
+  reverseArray.join(" ")
+end
+
+def reverse_each_word(sentence)
+  sentence.collect do | word |
+    word.reverse
+  end 
+  sentence.join (" ")
+end 
